9c18a73cd2ff5bddc783b80a8a3f1a4212abf45b
Esau Rodriguez <esauro@gmail.com>
2013-05-07 08:50:47 +0100

    Primera versión de nuestro amazing_blog

---
+++ amazing_blog/blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    intro = models.TextField()
+    mas = models.TextField(null = True, blank = True)
+
+    def __unicode__(self):
+        return self.title
