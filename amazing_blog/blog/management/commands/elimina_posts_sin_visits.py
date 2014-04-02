b25634046f56648777c105999fb2111286a79d47
Esau Rodriguez <esauro@gmail.com>
2014-04-02 06:12:49 +0100

    Mejoras estéticas al comando

--- amazing_blog/blog/management/commands/elimina_posts_sin_visits.py
+++ amazing_blog/blog/management/commands/elimina_posts_sin_visits.py
@@ -2,11 +2,9 @@ from django.core.management.base import BaseCommand
 from blog.models import Post, Visits
 
 class Command(BaseCommand):
-    args = '<poll_id poll_id ...>'
-    help = 'Closes the specified poll for voting'
+    help = 'Elimina los posts sin contador de visitas'
 
     def handle(self, *args, **options):
         for post in Post.objects.all():
             if not post.visits_set.all():
                 post.delete()
-        pass
