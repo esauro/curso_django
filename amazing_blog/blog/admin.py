7d9610dbb240814680f6ea9672efe72fe760c76c
Esau Rodriguez <esauro@gmail.com>
2014-04-02 05:09:54 +0100

    Añadido el nuevo user model a la interfaz administrativa

--- amazing_blog/blog/admin.py
+++ amazing_blog/blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from models import Post, Visits
+from models import Post, Visits, CustomUser
 
 
 admin.site.register(Post)
 admin.site.register(Visits)
+admin.site.register(CustomUser)
